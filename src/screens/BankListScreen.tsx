260c2a8b450159bb61ce4c3c24a33bc66b5a0213
Jonthan Casillas <react.jonathan.casillas@gmail.com>
2022-04-21 12:34:16 -0500

    se instalo navigation y axios

---
+++ src/screens/BankListScreen.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const BankListScreen = () => {
+  return (
+    <View>
+      <Text>BankListScreen</Text>
+    </View>
+  )
+}
+
+export default BankListScreen
+
+const styles = StyleSheet.create({})
